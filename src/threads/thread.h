7ae1ddebac3c7ef4c16fe2d22ee89bec2f18ae73
rapkt <zjarrar2005@gmail.com>
2026-05-04 02:57:13 +0300

    process execute

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -1,28 +1,27 @@
 #ifndef THREADS_THREAD_H
 #define THREADS_THREAD_H
 
+#include "threads/synch.h"
 #include <debug.h>
 #include <list.h>
 #include <stdint.h>
-#include "threads/synch.h"
 /* States in a thread's life cycle. */
-enum thread_status
-  {
-    THREAD_RUNNING,     /* Running thread. */
-    THREAD_READY,       /* Not running but ready to run. */
-    THREAD_BLOCKED,     /* Waiting for an event to trigger. */
-    THREAD_DYING        /* About to be destroyed. */
-  };
+enum thread_status {
+  THREAD_RUNNING, /* Running thread. */
+  THREAD_READY,   /* Not running but ready to run. */
+  THREAD_BLOCKED, /* Waiting for an event to trigger. */
+  THREAD_DYING    /* About to be destroyed. */
+};
 
 /* Thread identifier type.
    You can redefine this to whatever type you like. */
 typedef int tid_t;
-#define TID_ERROR ((tid_t) -1)          /* Error value for tid_t. */
+#define TID_ERROR ((tid_t) - 1) /* Error value for tid_t. */
 
 /* Thread priorities. */
-#define PRI_MIN 0                       /* Lowest priority. */
-#define PRI_DEFAULT 31                  /* Default priority. */
-#define PRI_MAX 63                      /* Highest priority. */
+#define PRI_MIN 0      /* Lowest priority. */
+#define PRI_DEFAULT 31 /* Default priority. */
+#define PRI_MAX 63     /* Highest priority. */
 
 /* Thread donation limit. */
 #define THREAD_DONATION_LIMIT 20
@@ -83,89 +82,92 @@ typedef int tid_t;
    only because they are mutually exclusive: only a thread in the
    ready state is on the run queue, whereas only a thread in the
    blocked state is on a semaphore wait list. */
-struct thread
-  {
-    /* Owned by thread.c. */
-    tid_t tid;                          /* Thread identifier. */
-    enum thread_status status;          /* Thread state. */
-    char name[16];                      /* Name (for debugging purposes). */
-    uint8_t *stack;                     /* Saved stack pointer. */
-    int priority;                       /* Priority. */
-    struct list_elem allelem;           /* List element for all threads list. */
-    int recent_cpu;
-    int nice;
-    /* Priority Donation */
-    int initial_priority;  
-    struct lock *waiting_on;
-    struct list donations;
-    struct list_elem donation_elem;
-    /* Shared between thread.c and synch.c. */
-    struct list_elem elem;              /* List element. */
+struct thread {
+  /* Owned by thread.c. */
+  tid_t tid;                 /* Thread identifier. */
+  enum thread_status status; /* Thread state. */
+  char name[16];             /* Name (for debugging purposes). */
+  uint8_t *stack;            /* Saved stack pointer. */
+  int priority;              /* Priority. */
+  struct list_elem allelem;  /* List element for all threads list. */
+  int recent_cpu;
+  int nice;
+  int exit_status; /* Process exit status. 0 if process exited successfully, -1
+                      otherwise. */
+  /* Priority Donation */
+  int initial_priority;
+  struct lock *waiting_on;
+  struct list donations;
+  struct list_elem donation_elem;
+  /* Shared between thread.c and synch.c. */
+  struct list_elem elem; /* List element. */
 
-    // Gamal: list element for sleeping threads list
-    struct list_elem s_elem;
+  // Gamal: list element for sleeping threads list
+  struct list_elem s_elem;
 
-    // Gamal: wake up time for sleeping thread
-    int64_t waketime;
+  // Gamal: wake up time for sleeping thread
+  int64_t waketime;
 
-    // Gamal: sleep semaphore
-    struct semaphore sema_sleep;
+  // Gamal: sleep semaphore
+  struct semaphore sema_sleep;
 
 #ifdef USERPROG
-    /* Owned by userprog/process.c. */
-    uint32_t *pagedir;                  /* Page directory. */
+  /* Owned by userprog/process.c. */
+  uint32_t *pagedir; /* Page directory. */
 #endif
 
-    /* Owned by thread.c. */
-    unsigned magic;                     /* Detects stack overflow. */
-  };
+  /* Owned by thread.c. */
+  unsigned magic; /* Detects stack overflow. */
+};
 
 /* If false (default), use round-robin scheduler.
    If true, use multi-level feedback queue scheduler.
    Controlled by kernel command-line option "-o mlfqs". */
 extern bool thread_mlfqs;
 
-void thread_init (void);
-void thread_start (void);
+void thread_init(void);
+void thread_start(void);
 
-void thread_tick (void);
-void thread_print_stats (void);
+void thread_tick(void);
+void thread_print_stats(void);
 
-typedef void thread_func (void *aux);
-tid_t thread_create (const char *name, int priority, thread_func *, void *);
+typedef void thread_func(void *aux);
+tid_t thread_create(const char *name, int priority, thread_func *, void *);
 
-void thread_block (void);
-void thread_unblock (struct thread *);
+void thread_block(void);
+void thread_unblock(struct thread *);
 
-struct thread *thread_current (void);
-tid_t thread_tid (void);
-const char *thread_name (void);
+struct thread *thread_current(void);
+tid_t thread_tid(void);
+const char *thread_name(void);
 
-void thread_exit (void) NO_RETURN;
-void thread_yield (void);
+void thread_exit(void) NO_RETURN;
+void thread_yield(void);
 
 /* Performs some operation on thread t, given auxiliary data AUX. */
-typedef void thread_action_func (struct thread *t, void *aux);
-void thread_foreach (thread_action_func *, void *);
+typedef void thread_action_func(struct thread *t, void *aux);
+void thread_foreach(thread_action_func *, void *);
 
-bool thread_priority_less (const struct list_elem *, const struct list_elem *, void *aux);
+bool thread_priority_less(const struct list_elem *, const struct list_elem *,
+                          void *aux);
 
-int thread_get_priority (void);
-void thread_set_priority (int);
+int thread_get_priority(void);
+void thread_set_priority(int);
 
-int thread_get_nice (void);
-void thread_set_nice (int);
-int thread_get_recent_cpu (void);
-int thread_get_load_avg (void);
+int thread_get_nice(void);
+void thread_set_nice(int);
+int thread_get_recent_cpu(void);
+int thread_get_load_avg(void);
 
 void donate_priority(void);
 void recalculate_priority(void);
 
-void mlfqs_recalculate_All_priority (struct thread *t,void *aux);
-void mlfqs_calculate_priority (struct thread *t);
-void mlfqs_recalculate_recent_cpu (struct thread *t);
+void mlfqs_recalculate_All_priority(struct thread *t, void *aux);
+void mlfqs_calculate_priority(struct thread *t);
+void mlfqs_recalculate_recent_cpu(struct thread *t);
 
 // Gamal: sleeping thread insertion comparator
-bool insert_sleeping_thread(struct list_elem *a, struct list_elem *b, void *aux UNUSED);
+bool insert_sleeping_thread(struct list_elem *a, struct list_elem *b,
+                            void *aux UNUSED);
 
 #endif /* threads/thread.h */
